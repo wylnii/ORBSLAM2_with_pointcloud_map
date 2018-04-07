c27c02a9b52de1a6420c5678dcd3b789a6f3e71f
wylnii <wylnii@163.com>
2018-04-07 15:21:46 +0800

    first commit

---
+++ Examples/ROS/ORB_SLAM2/README.md
@@ -0,0 +1,49 @@
+##FIXBUG
+1.修改ORB_SLAM2源码，更改图片及点云地图显示为彩色，增加自动保存result.pcd
+
+2.**FIX** 运行结束卡死bug
+
+3.**FIX** *Examples/ROS/ORB_SLAM2*编译失败（在*CMakeLists.txt*中加入PCL依赖等即可编译成功）
+
+4.**FIX** PointCloudMapping不完全更新的问题，当发生回环后不能更新校正后的点云地图
+
+5.**ADD** 点云地图PassThrough滤波，去除远处不准确的点
+
+##NOTE
+1.编译前需运行：
+```bash
+	export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/your/path/ORBSLAM2_with_pointcloud_map/ORB_SLAM2_modified/Examples/ROS
+```
+否则会出错说找不到路径和包
+
+2.运行
+```bash
+tools/bin_vocabulary
+```
+生成二进制字典
+
+3.停止运行病保存生成的轨迹与点云地图，请运行
+```bash 
+rostopic pub /RGBD/cmd std_msgs/String "data: 'stop'"
+```
+
+##How To Build
+
+1. ```export ROS_PACKAGE_PATH=${ROS_PACKAGE_PATH}:/your/path/ORBSLAM2_with_pointcloud_map/ORB_SLAM2_modified/Examples/ROS```
+
+2. ```mkdir build```
+
+3. ```cmake ..```
+ 
+4. ```make -j4```
+
+##How To Run
+
+1. ```rosbag play -r 1.0 *.bag```
+  或使用Kinect
+  
+2. ```./RGBD ../../../Vocabulary/ORBvoc.bin kinect2_121717234447.yaml```
+
+3. ```rostopic pub /RGBD/cmd std_msgs/String "data: 'stop'"``` 停止建图
+
+4. ```pcl_viewer result.pcd```查看pcd文件
---
+++ src/pointcloudmapping.cc
@@ -0,0 +1,191 @@
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2016  <copyright holder> <email>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "pointcloudmapping.h"
+#include <KeyFrame.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/common/projection_matrix.h>
+#include <pcl/io/pcd_io.h>
+#include "Converter.h"
+
+#include <boost/make_shared.hpp>
+
+PointCloudMapping::PointCloudMapping(float resolution_, Map *map) : mpMap(map)
+{
+    this->resolution = resolution_;
+    voxel.setLeafSize(resolution, resolution, resolution);
+    pass.setFilterFieldName("z");
+    pass.setFilterLimits(0.0, 3.0);
+    globalMap = boost::make_shared<PointCloud>();
+
+    viewerThread = make_shared<thread>(bind(&PointCloudMapping::viewer, this));
+}
+
+void PointCloudMapping::shutdown()
+{
+    {
+        unique_lock<mutex> lck(shutDownMutex);
+        shutDownFlag = true;
+        keyFrameUpdated.notify_one();
+    }
+    viewerThread->join();
+}
+
+void PointCloudMapping::insertKeyFrame(KeyFrame *kf, cv::Mat &color, cv::Mat &depth)
+{
+    cout << kf->mTimeStamp << " receive a keyframe, id = " << kf->mnId << endl;
+    unique_lock<mutex> lck(keyframeMutex);
+    keyframes.push_back(kf);
+    colorImgs.push_back(color.clone());
+    depthImgs.push_back(depth.clone());
+
+    keyFrameUpdated.notify_one();
+}
+
+void PointCloudMapping::insertKeyFrame(KeyFrame *kf)
+{
+    cout << kf->mTimeStamp << " receive a keyframe, id = " << kf->mnId << endl;
+    unique_lock<mutex> lck(keyframeMutex);
+
+    keyFrameUpdated.notify_one();
+}
+
+pcl::PointCloud<PointCloudMapping::PointT>::Ptr
+PointCloudMapping::generatePointCloud(KeyFrame *kf, cv::Mat &color, cv::Mat &depth)
+{
+    PointCloud::Ptr tmp(new PointCloud());
+    // point cloud is null ptr
+    for (int m = 0; m < depth.rows; m += 3)
+    {
+        for (int n = 0; n < depth.cols; n += 3)
+        {
+            float d = depth.ptr<float>(m)[n];
+            if (d < 0.01 || d > 10)
+                continue;
+            PointT p;
+            p.z = d;
+            p.x = (n - kf->cx) * p.z / kf->fx;
+            p.y = (m - kf->cy) * p.z / kf->fy;
+
+            p.b = color.ptr<uchar>(m)[n * 3];
+            p.g = color.ptr<uchar>(m)[n * 3 + 1];
+            p.r = color.ptr<uchar>(m)[n * 3 + 2];
+
+            tmp->points.push_back(p);
+        }
+    }
+
+    Eigen::Isometry3d T = ORB_SLAM2::Converter::toSE3Quat(kf->GetPose());
+    PointCloud::Ptr cloud(new PointCloud);
+    pass.setInputCloud(tmp);
+    pass.filter(*cloud);
+    tmp->swap(*cloud);
+
+    pcl::transformPointCloud(*tmp, *cloud, T.inverse().matrix());
+    cloud->is_dense = false;
+
+    cout << "generate point cloud for kf " << kf->mnId << ", size=" << cloud->points.size() << endl;
+    return cloud;
+}
+
+
+void PointCloudMapping::viewer()
+{
+    pcl::visualization::CloudViewer viewer("PointCloud Viewer");
+
+    while (true)
+    {
+        {
+            unique_lock<mutex> lck_shutdown(shutDownMutex);
+            if (shutDownFlag)
+            {
+                break;
+            }
+        }
+        {
+            unique_lock<mutex> lck_keyframeUpdated(keyFrameUpdateMutex);
+            keyFrameUpdated.wait(lck_keyframeUpdated);
+        }
+
+
+        // keyframe is updated
+//        keyframes = mpMap->GetAllKeyFrames();
+//        size_t N = 0;
+//        {
+//            unique_lock<mutex> lck(keyframeMutex);
+//            N = keyframes.size();
+//        }
+//
+//        for (size_t i = lastKeyframeSize; i < N; i++)
+//        {
+//            PointCloud::Ptr p = generatePointCloud(keyframes[i], keyframes[i]->img0, keyframes[i]->img1);
+//            *globalMap += *p;
+//        }
+//
+//        PointCloud::Ptr tmp(new PointCloud());
+//        voxel.setInputCloud(globalMap);
+//        voxel.filter(*tmp);
+//
+//        globalMap->swap(*tmp);
+
+        generateMap();
+        viewer.showCloud(globalMap);
+
+        cout << "show global map, size=" << globalMap->points.size() << endl;
+//        lastKeyframeSize = N;
+    }
+    savePointCloud("result.pcd");
+    cout << "pcl_viewer shutdown!" << endl;
+}
+
+void PointCloudMapping::savePointCloud(const string &name)
+{
+//    generateMap();
+    if (!globalMap->empty())
+    {
+        pcl::io::savePCDFile(name, *globalMap, true);
+        cout << "save map to file : " << name << endl;
+    }
+}
+
+void PointCloudMapping::generateMap()
+{
+    keyframes = mpMap->GetAllKeyFrames();
+    globalMap->clear();
+    size_t N = 0;
+    {
+        unique_lock<mutex> lck(keyframeMutex);
+        N = keyframes.size();
+    }
+
+    for (size_t i = 0; i < N; i++)
+    {
+        PointCloud::Ptr p = generatePointCloud(keyframes[i], keyframes[i]->img0, keyframes[i]->img1);
+        *globalMap += *p;
+    }
+
+    PointCloud::Ptr tmp(new PointCloud());
+    voxel.setInputCloud(globalMap);
+    voxel.filter(*tmp);
+
+    globalMap->swap(*tmp);
+
+    cout << "show global map, size=" << globalMap->points.size() << endl;
+
+}
